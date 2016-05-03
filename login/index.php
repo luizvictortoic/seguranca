22b4873af93c7507f8be388f3324fad138560bb4
Luiz Victor Zumba <luizvictortoic@hotmail.com>
2016-05-03 17:22:35 -0300

    Tela de Login

---
+++ login/index.php
@@ -0,0 +1,6 @@
+<form> action="">
+	Login: <input type="text" /> <br>
+	Senha: <input type="password" /> <br>
+	<input type="checkbox"> Esqueceu a Senha
+	<input type="submit" value="Logar">
+</form>
